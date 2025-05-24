7ecd2ea13e927dff2c1d97147061a578c49cfdb7
Vinay Kumar <mmvinaykumar.mm@gmail.com>
2025-05-24 23:56:49 +0200

    About app and about developer added.

--- app/src/main/java/com/example/alertuser/MainActivity.java
+++ app/src/main/java/com/example/alertuser/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.alertuser;
 
 import android.app.AppOpsManager;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     Button startStopButton;
     Switch switchTimer;
     Boolean timerFlag;
-    TextView textViewTimer;
+    TextView textViewTimer, aboutDeveloper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +31,19 @@ public class MainActivity extends AppCompatActivity {
         startStopButton = findViewById(R.id.startStopButton);
         switchTimer = findViewById(R.id.switch_timer);
         textViewTimer = findViewById(R.id.textViewTimer);
+        aboutDeveloper = findViewById(R.id.aboutDeveloper);
 
         SharedPreferences prefs = getSharedPreferences("AppPrefs", MODE_PRIVATE);
         timerFlag = prefs.getBoolean("timer_enabled", true);
         int currTimer = prefs.getInt("TIMER_MINUTES", 0);
         SharedPreferences.Editor editor = prefs.edit();
 
-        textViewTimer.setText(currTimer);
-        if(timerFlag) {
+        textViewTimer.setText(currTimer + " minutes");
+        if (timerFlag) {
             switchTimer.setChecked(true);
             switchTimer.setText("Enable Timer");
             startStopButton.setText("Start");
-        }
-        else {
+        } else {
             switchTimer.setChecked(false);
             switchTimer.setText("Disable Timer");
             startStopButton.setText("Stop");
@@ -56,8 +58,7 @@ public class MainActivity extends AppCompatActivity {
             if (isChecked) {
                 switchTimer.setText("Enable Timer");
                 startStopButton.setText("Start");
-            }
-            else {
+            } else {
                 switchTimer.setText("Disable Timer");
                 startStopButton.setText("Stop");
             }
@@ -76,19 +77,64 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
 
-            int minutes = Integer.parseInt(timerEditText.getText().toString());
-            editor.putInt("TIMER_MINUTES", minutes).apply();
-
             Intent intent = new Intent(this, UsageMonitorService.class);
-            //intent.putExtra("TIMER_MINUTES", minutes);
+            int minutes = 0;
 
-            if (timerFlag) {
-
+            if (timerFlag && !timerEditText.getText().toString().isEmpty()) {
+                minutes = Integer.parseInt(timerEditText.getText().toString());
+                editor.putInt("TIMER_MINUTES", minutes).apply();
+                textViewTimer.setText(minutes + " minute/s");
                 startForegroundService(intent);
             }
-            else
+
+            if (!timerFlag) {
+                textViewTimer.setText(" - ");
                 stopService(intent);
+            }
         });
+
+        TextView aboutDeveloper = findViewById(R.id.aboutDeveloper);
+        TextView aboutApp = findViewById(R.id.aboutApp);
+
+        aboutDeveloper.setOnClickListener(v -> {
+            final Dialog aboutDevDialog = new Dialog(MainActivity.this);
+            aboutDevDialog.setContentView(R.layout.dialog_about_developer);
+            aboutDevDialog.setCancelable(true);
+
+            ImageView emailIcon = aboutDevDialog.findViewById(R.id.emailIcon);
+            ImageView linkedinIcon = aboutDevDialog.findViewById(R.id.linkedinIcon);
+            Button btnClose = aboutDevDialog.findViewById(R.id.closeButton);
+
+            emailIcon.setOnClickListener(view -> {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse("mailto:mmvinaykumar.mm@gmail.com"));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "App Contact");
+                startActivity(Intent.createChooser(emailIntent, "Send email"));
+            });
+
+            linkedinIcon.setOnClickListener(view -> {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.linkedin.com/in/vinaykumar-mysuru-manjunath-33b522ba/"));
+                startActivity(browserIntent);
+            });
+
+            btnClose.setOnClickListener(view -> aboutDevDialog.dismiss());
+
+            aboutDevDialog.show();
+        });
+
+        aboutApp.setOnClickListener(v -> {
+            final Dialog aboutAppDialog = new Dialog(MainActivity.this);
+            aboutAppDialog.setContentView(R.layout.dialog_about_app);
+            aboutAppDialog.setCancelable(true);
+
+            Button btnClose = aboutAppDialog.findViewById(R.id.closeButton);
+
+            btnClose.setOnClickListener(view -> aboutAppDialog.dismiss());
+
+            aboutAppDialog.show();
+        });
+
     }
 
     private boolean hasUsageAccessPermission() {
